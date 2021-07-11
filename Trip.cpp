06c9946f17a96db0852df142e14628b7fa99c323
Alexis <muroa1@uci.edu>
2021-07-11 12:55:35 -0700

    Complete Trip class and update Trip documentation

--- Trip.cpp
+++ Trip.cpp
@@ -8,28 +8,45 @@
 
 #include "Trip.h"
 
-Trip::Trip(std::string name, long double budget, long double warningLimit){
+Trip::Trip(const std::string& name, long double budget, long double warningLimit): name(name), budget(budget), balance(budget), warningLimit(warningLimit)
+{
     if(name.empty()) throw std::invalid_argument("Name cannot be empty");
     if(budget < 1 || warningLimit < 1) throw std::invalid_argument("Budget and warning limit must be at least 1");
-    if(warningLimit > budget) throw std::invalid_argument("Warning limit cannot be greater than budget");
-    this->name = name;
-    this->budget = budget;
-    this->balance = budget;
-    this->warningLimit = warningLimit;
+    if(warningLimit >= budget) throw std::invalid_argument("Warning limit cannot be greater or equal to budget");
 }
 
-long double Trip::getBudget() {
+long double Trip::getBudget() const {
     return budget;
 }
 
-long double Trip::getBalance() {
+long double Trip::getBalance() const {
     return balance;
 }
 
-long double Trip::getWarningLimit() {
+long double Trip::getWarningLimit() const {
     return warningLimit;
 }
 
-std::string Trip::getName() {
+std::string Trip::getName() const {
     return name;
+}
+
+bool Trip::isOverBudget() const {
+    return balance < 0;
+}
+
+bool Trip::isWarningActive() const {
+    return balance <= warningLimit;
+}
+
+std::vector<std::pair<std::string,long double>> Trip::getExpenses() const {
+    return expenses;
+}
+
+void Trip::addExpense(std::string name, long double amount) {
+    if(name.empty()) throw std::invalid_argument("Name cannot be empty");
+    if(amount < 0) throw std::invalid_argument("Amount must be greater than 0");
+    balance -= amount;
+    std::pair<std::string, long double> expense(name, amount);
+    expenses.push_back(expense);
 }
--- Trip.h
+++ Trip.h
@@ -17,13 +17,13 @@
 class Trip {
 private:
     /** Name of trip **/
-    std::string name;
+    const std::string name;
     /** Budget allocated for trip **/
-    long double budget = 0;
+    const long double budget = 0;
     /** Balance remaining for the trip **/
     long double balance = 0;
     /** Warning limit allocated for trip **/
-    long double warningLimit = 0;
+    const long double warningLimit = 0;
     /**
      * List of expenses for the trip.
      * Expenses will be stored as pairs, where the first value of the pair holds: the name of the expense
@@ -38,43 +38,66 @@ public:
      * @param name the name of the trip
      * @param budget the total budget allocated for this trip
      * @param warningLimit the amount where a warning will be sent out to user
-     * @throws invalid_argument if name is empty, if {@code budget < 1}, and if {@code warningLimit < 1 || warningLimit > budget}
+     * @throws invalid_argument if name is empty, if {@code budget < 1}, and if {@code warningLimit < 1 || warningLimit >= budget}
      */
-    Trip(std::string name, long double budget, long double warningLimit);
+    Trip(const std::string& name, long double budget, long double warningLimit);
 
     /**
      * Gets the starting budget for the trip
      *
      * @return the initial budget for the trip
      */
-    long double getBudget();
+    [[nodiscard]] long double getBudget() const;
 
     /**
      * Gets the current balance
      *
      * @return the remaining balance for the trip
      */
-    long double getBalance();
+    [[nodiscard]] long double getBalance() const;
 
     /**
      * Gets the warning limit that was set for the trip
      *
      * @return the warning limit amount
      */
-    long double getWarningLimit();
+    [[nodiscard]] long double getWarningLimit() const;
 
     /**
      * Gets the name of the trip
      *
      * @return the name of trip
      */
-    std::string getName();
+    [[nodiscard]] std::string getName() const;
+
+    /**
+     * Check if the balance is below 0
+     *
+     * @return {@code true} if {@code balance < 0}
+     *         {@code false} if {@code balance >= 0}
+     */
+    [[nodiscard]] bool isOverBudget() const;
+
+    /**
+     * Check if the balance has reached the warning limit
+     *
+     * @return {@code true} if {@code balance <= warningLimit}
+     *         {@code false} if {@code balance > warningLimit}
+     */
+    [[nodiscard]] bool isWarningActive() const;
+
+    /**
+     * Get the list of expenses for the trip
+     *
+     * @return the vector that holds the expenses
+     */
+    [[nodiscard]] std::vector<std::pair<std::string,long double>> getExpenses() const;
 
     /**
      * Adds an expense to the list of expenses for this trip
      *
      * @param amount
-     * @throws illegal_argument if name or amount is NULL
+     * @throws invalid_argument if name is empty or if {@code amount < 0}
      */
     void addExpense(std::string name, long double amount);
 };
